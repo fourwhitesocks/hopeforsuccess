9c3375df0bfff99c5306d4d77aa7ba9b018ab716
fourwhitesocks <asbilly92@gmail.com>
2021-12-24 12:03:38 -0500

    feat: :sparkles: Add GSAP!  to Burger page

---
+++ src/lib/gsap.js
@@ -0,0 +1,7 @@
+
+export function moveDown() {
+
+gsap.to('.text' , {y: 50, duration: 1})
+
+}
+
--- src/routes/burger.svelte
+++ src/routes/burger.svelte
@@ -1,8 +1,30 @@
 <script>
 	import BurgerMenu from 'svelte-burger-menu';
+	import { onMount } from 'svelte';
+	import { moveDown } from '../lib/gsap';
+
+	onMount(()=> {
+
+		moveDown()
+
+	}
+
+
+	)
+
+
 </script>
 
 <BurgerMenu>
 	<h2>Home</h2>
 	<h2>About</h2>
 </BurgerMenu>
+
+<h1 class="text">Hello GSAP is very moving!</h1>
+
+
+
+
+<style>
+	h2 {color: white;} 
+</style>
