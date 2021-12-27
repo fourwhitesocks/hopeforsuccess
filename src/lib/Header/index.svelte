bf577878f799e6e6ba668d1d284eae511c0844e0
amy stout <asbilly92@gmail.com>
2021-12-27 13:48:18 -0500

    refactor: :fire: removed clock from nav

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -18,7 +18,7 @@
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-			<li class:active={$page.path === '/clock'}><a sveltekit:prefetch href="/clock">Clock</a></li>
+
 			<li class:active={$page.path === '/burger'}>
 				<a sveltekit:prefetch href="/burger">Burger</a>
 			</li>
